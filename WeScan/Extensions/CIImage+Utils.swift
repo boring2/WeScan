4d8a607b094d8a6d6dca2a6f1157110a4a269a3e
Mr.Boring <code.zhen@gmail.com>
2019-08-26 10:58:08 +0800

    add applyingPhotoEffectMono

--- WeScan/Extensions/CIImage+Utils.swift
+++ WeScan/Extensions/CIImage+Utils.swift
@@ -36,4 +36,14 @@ extension CIImage {
             return UIImage(ciImage: enhancedCIImage, scale: 1.0, orientation: .up)
         }
     }
+    func applyingPhotoEffectMono() -> UIImage? {
+      let context = CIContext(options: nil)
+      guard let currentFilter = CIFilter(name: "CIPhotoEffectMono") else { return nil }
+      currentFilter.setValue(self, forKey: kCIInputImageKey)
+      if let output = currentFilter.outputImage,
+        let cgImage = context.createCGImage(output, from: output.extent) {
+        return UIImage(cgImage: cgImage)
+      }
+      return nil
+    }
 }
